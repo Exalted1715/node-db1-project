34133e351ef8d287932bf6c05fc6c9333c46e6c0
Mitch Hammond <mitch.b.hammond@gmail.com>
2024-04-11 10:14:57 -0600

    updates to router accounts

--- api/server.js
+++ api/server.js
@@ -1,15 +1,18 @@
 const express = require("express");
 
+const accountsRouter = require('./accounts/accounts-router')_
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', accountsRouter)
+
 
 server.use('*',(req,res)=>{
     res.status(404).json({
         message: 'not found',
     })
 })
-
-
 
 module.exports = server;
