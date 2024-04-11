114e36edceafedaf18bd1d1b0c2f58edc6fd947f
Mitch Hammond <mitch.b.hammond@gmail.com>
2024-04-11 10:33:22 -0600

    middleware connected

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,13 +1,15 @@
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-  // Note: you can either write "manual" validation logic
-  // or use the Yup library (not currently installed)
+  console.log('checkAccountPayload middleware')
+  next()
+
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+  next(console.log('checkAccountNameUnique middleware'))
+
 }
 
 exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+  next(console.log('checkAccountId middleware'))
+
 }
